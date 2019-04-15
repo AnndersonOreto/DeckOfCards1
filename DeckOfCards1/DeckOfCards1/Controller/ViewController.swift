68abf1a8d5a33ad6b3fe51475b5f929739838438
Annderson Pack <annderson.oreto@acad.pucrs.br>
2019-04-15 15:35:05 -0300

    ViewController

--- DeckOfCards1/DeckOfCards1/Controller/ViewController.swift
+++ DeckOfCards1/DeckOfCards1/Controller/ViewController.swift
@@ -13,9 +13,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         print("😁")
     }
-
+    
+    func drawCard() {
+        
+        let urlString = URL(string: "")
+        
+        if let url = urlString {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error)
+                } else {
+                    if let usableData = data {
+                        print(usableData) //JSONSerialization
+                    }
+                }
+            }
+            task.resume()
+        }
+        
+    }
 
 }
+
 
