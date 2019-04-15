99e4b26e1aa1ce6cec9ea409b062f65fade3087c
Annderson Pack <annderson.oreto@acad.pucrs.br>
2019-04-15 14:00:40 -0300

    Start

--- DeckOfCards1/DeckOfCards1/Controller/ViewController.swift
+++ DeckOfCards1/DeckOfCards1/Controller/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var hand: [Card] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ DeckOfCards1/DeckOfCards1/Model/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  DeckOfCards1
+//
+//  Created by Annderson Packeiser Oreto on 15/04/19.
+//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    init() { }
+    
+}
